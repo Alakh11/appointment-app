6222448d1b84606b5aa30d8eb03e69b5a0776720
Alakh Chaturvedi <alakh1304@gmail.com>
2024-10-15 22:05:20 +0530

    Using ng ONinit

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+
 newAppointmentTitle: String ="";
 newAppointmentDate : Date = new Date();
 appointments: Appointment[] =  []
+
+ngOnInit(): void {
+  let savedAppointments = localStorage.getItem("appointments")
+  this.appointments = savedAppointments ? JSON.parse(savedAppointments): []
+  //console.log("got loaded")
+ // throw new Error('Method not implemented.');
+}
 
 addAppointment(){
   if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
